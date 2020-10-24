b7f46eedf6534cb7124fc9a26ac2185afe352b75
keyian <keyian@nyu.edu>
2020-10-24 17:25:39 -0400

    testing automatic deployment. change to Title.

--- client/src/App.js
+++ client/src/App.js
@@ -83,7 +83,7 @@ class App extends React.Component {
     //JSX
     return(
       <div className="app">
-        <h2>Welcome to My App</h2>
+        <h2>Testing the ol pushie pushie</h2>
         <form onSubmit={this.submit}>
           <div className="form-input">
             <input
